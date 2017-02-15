8c0159f9b456ff204c994790886ed4aec1f0502e
Kortine Kleinheinz <k.kleinheinz@dkfz-heidelberg.de>
2017-02-15 15:11:55 +0100

    sample variable as other RSCRIPT_BINARY values to make identical

--- resources/analysisTools/copyNumberEstimationWorkflow/estimateGenotypes.sh
+++ resources/analysisTools/copyNumberEstimationWorkflow/estimateGenotypes.sh
@@ -5,7 +5,7 @@ source $CONFIG_FILE
 #change header and index file
 
 #get pseudo control counts
-${RSCRIPT_BINARY_3} $TOOL_GET_HET_SNPS -s ${FILENAME_SNP_POSITIONS_WG} | bgzip >${FILENAME_SNP_POSITIONS_WG_FAKE}.tmp
+${RSCRIPT_BINARY} $TOOL_GET_HET_SNPS -s ${FILENAME_SNP_POSITIONS_WG} | bgzip >${FILENAME_SNP_POSITIONS_WG_FAKE}.tmp
 ${TABIX_BINARY} -h -f -s 1 -b 2 -e 2 ${FILENAME_SNP_POSITIONS_WG_FAKE}.tmp
 
 if [[ $? != 0 ]]
