08ac78bbfa8d1a2eccd004568c318ab1dd524be5
Kortine Kleinheinz <k.kleinheinz@dkfz-heidelberg.de>
2017-02-23 11:09:51 +0100

    addition of BAF plots for control

---
+++ resources/analysisTools/copyNumberEstimationWorkflow/createBAFplots.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+source ${CONFIG_FILE}
+
+
+${RSCRIPT_BINARY} ${TOOL_CREATE_BAF_PLOTS} --file_snp  ${FILENAME_SNP_POSITIONS_HAPLO_WG} \
+					   --file_sex  ${FILENAME_SEX} \ 
+					   --chrLengthFile ${CHROMOSOME_LENGTH_FILE}} \
+					   --pid ${PID} \
+					   --plot_Dir ${plotOutputDirectory}
+
+if [[ "$?" != 0 ]]
+then
+        echo "There was an error during BAF plot creation;"
+        exit 2
+fi
+
+touch ${FILENAME_BAFPLOT_CHECKPOINT}
