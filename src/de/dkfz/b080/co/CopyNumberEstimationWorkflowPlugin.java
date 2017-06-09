53f1a5fb1a87eb4ff89a34e3ab154829fec37d8e
Kortine Kleinheinz <k.kleinheinz@dkfz-heidelberg.de>
2017-06-09 17:48:20 +0200

    documented updates in workflow
    Summary:
    pscbs_plots_functions.R
    
    addition of annotateCNA function, new estimation of qual parameter, tcc instead of normalContamination in json, removal of GNL column, replace purity with tcc in all code snippets
    
    addition of annotateCNA function, new estimation of qual parameter, tcc instead of normalContamination in json, removal of GNL column, replace purity with tcc in all code snippets
    
    version increase and documentation
    
    minor bugfix COConstants now ACEseqConstants
    
    new compliation
    
    Test Plan: differential
    
    Reviewers: heinold, kensche
    
    Subscribers: reisinge
    
    Differential Revision: https://eilslabs-phabricator.dkfz.de/D390

--- src/de/dkfz/b080/co/CopyNumberEstimationWorkflowPlugin.java
+++ src/de/dkfz/b080/co/CopyNumberEstimationWorkflowPlugin.java
@@ -8,8 +8,8 @@ import de.dkfz.roddy.plugins.BasePlugin;
  */
 public class CopyNumberEstimationWorkflowPlugin extends BasePlugin {
 
-    public static final String CURRENT_VERSION_STRING = "1.2.7";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Fri Apr 28 15:07:10 CEST 2017";
+    public static final String CURRENT_VERSION_STRING = "1.2.10";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Fri Jun 09 17:42:36 CEST 2017";
 
     @Override
     public String getVersionInfo() {
