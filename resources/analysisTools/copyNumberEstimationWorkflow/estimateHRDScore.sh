d3fecd17cf678114049540d70d4cb2a0b78f4a35
Kleinheinz <k.kleinheinz@dkfz-heidelberg.de>
2017-07-16 23:08:26 +0200

    adjust script so it will be run for all solutions possible

--- resources/analysisTools/copyNumberEstimationWorkflow/estimateHRDScore.sh
+++ resources/analysisTools/copyNumberEstimationWorkflow/estimateHRDScore.sh
@@ -1,37 +1,47 @@
 #!/usr/bin/sh
 
-path=/icgc/pcawg/analysis/train2full/projects/
-ACEseq=
-target=/icgc/pcawg/analysis/BRCAness_biomarker/cnv_new/newgenelists/
-genelistFile=/icgc/pcawg/analysis/BRCAness_biomarker/gene_lists/all_BRCAness.bed
-blacklists=/icgc/dkfzlsdf/analysis/mmml/genome/cohort_analysis/CNV_analysis_SOPHIA_v32/comb_pro_extra_SOPHIA/blacklistRegions
-TOOLSDIR=/home/kleinhei/Project/panCan/pancan.git/CNV_analysis/
+ACEseq=${aceseqOutputDirectory}
+blacklists=${ACEseqBlacklistRegions} #blacklistRegions
 
-date=`date +%y%m%d%H`
-		 [[  ! -f $path/$project/$pid/$ACEseq/checkpointVcf.txt ]] &&  continue;
 
-##remove artifact regions
-echo $pid
+FILENAME_PARAMETER_JSON=$1
+#$PYTHON_BINARY ${TOOL_PARSE_JSON} -f ${FILENAME_PARAMETER_JSON} | while read line
+python parseJson.py -f ${FILENAME_PARAMETER_JSON} | while read line
+do
+	solutions=$(line)
+	for item in ${solutions[@]}; do
+		eval $item
+	done
+	combProFile=${aceseqOutputDirectory}/${pid}_comb_pro_extra${ploidyFactor}_${tcc}.txt
+	mostImportantFile=${aceseqOutputDirectory}/${pid}_comb_pro_extra${ploidyFactor}_${tcc}.txt
+	##remove artifact regions
+	combProFile.new=`echo $combProFile | sed 's/.txt/.smoothed.txt/'`
+	combProFile.noArtifacts=`echo $combProFile | sed 's/.txt/.noartifacts.txt/'`
+	${BEDTOOLS_BINARY} intersect -header -v -f 0.7 \
+				     -a $combProFile -b $blacklists/artifact.homoDel1500.txt $file | \
+	${BEDTOOLS_BINARY} intersect -header -v -f 0.7 \
+				     -a - -b $blacklists/potentialArtifacts.txt \
+				     >$combProFile.tmp
 
-#smooth Data
-bedtools-2.24.0 intersect -header -v -f 0.7 -a - -b $blacklists/artifact.homoDel1500.txt $file | \
-bedtools-2.24.0 intersect -header -v -f 0.7 -a - -b $blacklists/potentialArtifacts.txt >$combProFile.tmp
+	#smooth Data
+	${PYTHON_BINARY} ${TOOL_REMOVE_BREAKPOINTS} -f $combProFile.tmp -o $combProFile.tmp.tmp && mv $combProFile.tmp.tmp $combProFile.tmp && \
+	${PYTHON_BINARY} ${TOOL_MERGE_ARTIFACTS} -f $combProFile.tmp -o $combProFile.tmp.tmp && mv $combProFile.tmp.tmp $combProFile.tmp && \
+	${PYTHON_BINARY} ${TOOL_REMOVE_BREAKPOINTS} -f $combProFile.tmp -o $combProFile.tmp.tmp && mv $combProFile.tmp.tmp $combProFile.tmp && \
 
-python ${TOOLSDIR}/removeBreakpoints.py -f $combProFile.tmp -i 4108101 -o $combProFile.tmp.tmp && mv $combProFile.tmp.tmp $combProFile.tmp && \
-python ${TOOLSDIR}/mergeArtifacts.py -f $combProFile.tmp -i 4108101 -o $combProFile.tmp.tmp && mv $combProFile.tmp.tmp $combProFile.tmp && \
-python ${TOOLSDIR}/removeBreakpoints.py -f $combProFile.tmp -i 4108101 -o $combProFile.tmp.tmp && mv $combProFile.tmp.tmp $combProFile.tmp && \
-#this file could be written out and sorted according to chromosomes
-python ${TOOLSDIR}/smoothData.py -f $combProFile.tmp  -o $combProFile.tmp.tmp && mv $combProFile.tmp.tmp $combProFile.tmp && \
-python ${TOOLSDIR}/removeBreakpoints.py -f $combProFile.tmp -i 4108101 -o $combProFile.tmp.tmp && mv $combProFile.tmp.tmp $combProFile.tmp
+	cp $combProFile.tmp $combProFile.noArtifacts
+	#this file could be written out and sorted according to chromosomes
+	${PYTHON_BINARY} ${TOOL_SMOOTH_DATA} -f $combProFile.tmp  -o $combProFile.tmp.tmp && mv $combProFile.tmp.tmp $combProFile.tmp && \
+	${PYTHON_BINARY} ${TOOL_REMOVE_BREAKPOINTS} -f $combProFile.tmp -o $combProFile.tmp.tmp && mv $combProFile.tmp.tmp $combProFile.tmp
 
-patientsex=`cat $SEX_FILE`
-fullploidy=`grep "fullPloidy" $file | sed 's/.*://'`
-purity=`grep "purity" $file | sed 's/.*://'`
+	patientsex=`cat $SEX_FILE`
 
-Rscript-3.3.1 /home/kleinhei/Project/panCan/pancan.git/CNV_analysis/analyseCNVs.new.R \
-        --patientsex $patientsex \
-	--ploidy $fullploidy \
-	--purity $purity \
-	--id $pid \
-	--segmentfile $file.tmp \
-	--mergedfile $combProFile.tmp 
+	${RSCRIPT_BINARY} ${TOOL_ESTIMATE_HRD} \
+		--patientsex $gender \
+		--ploidy $ploidy \
+		--tcc $tcc \
+		--id $pid \
+		--segmentfile $combProFile.noArtifacts \
+		--mergedfile $combProFile.tmp 
+
+	mv $combProFile.tmp $combProFileNew
+done
