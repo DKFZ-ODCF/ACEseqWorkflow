0a20e5c2f5f49fac398363810a7acb3d0a265aac
kleinhei <k.kleinheinz@dkfz-heidelberg.de>
2017-07-20 16:56:43 +0200

    parse json to get variables

---
+++ resources/analysisTools/copyNumberEstimationWorkflow/parseJson.py
@@ -0,0 +1,20 @@
+
+
+import json
+import argparse
+import sys
+
+parser=argparse.ArgumentParser(description='Convert tab seperated CNV calls to vcf file' )
+parser.add_argument('--file', '-f', type=file, help='Tabfile with headerline')
+
+parser.add_argument
+args = parser.parse_args()
+
+json_data=args.file.read()
+
+solutions =  json.loads(json_data)
+
+for index in solutions.keys():
+	for detail in solutions[index].keys():
+		sys.stdout.write( " " + detail + "=" + solutions[index][detail] +" ")
+	sys.stdout.write("\n")
