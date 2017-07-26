a05d6538635d517feacb25b832b1414cc37b2853
Kleinheinz <k.kleinheinz@dkfz-heidelberg.de>
2017-07-26 22:18:29 +0200

    reduce number back to 1.2.9

--- src/de/dkfz/b080/co/CopyNumberEstimationWorkflowPlugin.java
+++ src/de/dkfz/b080/co/CopyNumberEstimationWorkflowPlugin.java
@@ -8,8 +8,8 @@ import de.dkfz.roddy.plugins.BasePlugin;
  */
 public class CopyNumberEstimationWorkflowPlugin extends BasePlugin {
 
-    public static final String CURRENT_VERSION_STRING = "1.2.10";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Thu Jul 20 16:51:45 CEST 2017";
+    public static final String CURRENT_VERSION_STRING = "1.2.9";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Thu Jul 20 17:35:40 CEST 2017";
 
     @Override
     public String getVersionInfo() {
