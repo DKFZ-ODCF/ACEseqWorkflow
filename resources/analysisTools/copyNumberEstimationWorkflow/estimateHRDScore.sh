61ca8f47be5c47bd0bc70ff468aaf92fd1d6a926
Kleinheinz <k.kleinheinz@dkfz-heidelberg.de>
2017-07-26 22:17:32 +0200

    adjust scripts for modularization and introduce variables in bash script

--- resources/analysisTools/copyNumberEstimationWorkflow/estimateHRDScore.sh
+++ resources/analysisTools/copyNumberEstimationWorkflow/estimateHRDScore.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/sh
 
-source  ~/tmp.txt
-echo $aceseqOutputDirectory
+source ${CONFIG_FILE}
 
-FILENAME_PARAMETER_JSON=$1
-#$PYTHON_BINARY ${TOOL_PARSE_JSON} -f ${FILENAME_PARAMETER_JSON} | while read line
-python parseJson.py -f ${FILENAME_PARAMETER_JSON} | while read line
+PIPELINE_DIR=`dirname ${TOOL_HRD_ESTIMATION}`
+$PYTHON_BINARY ${TOOL_PARSE_JSON} -f ${FILENAME_PARAMETER_JSON} | while read line
 do
 	solutions=$(line)
 	for item in ${solutions[@]}; do
@@ -18,21 +16,44 @@ do
 	##remove artifact regions
 	combProFileNew=$(echo $combProFile | sed 's/.txt/.smoothed.txt/')
 	combProFileNoArtifacts=$(echo $combProFile | sed 's/.txt/.noartifacts.txt/')
-	${BEDTOOLS_BINARY} intersect -header -v -f 0.7 \
-				     -a $combProFile -b $blacklists/artifact.homoDel1500.txt | \
-	${BEDTOOLS_BINARY} intersect -header -v -f 0.7 \
-				     -a - -b $blacklists/potentialArtifacts.txt \
+	${INTERSECTBED_BINARY} -header -v -f 0.7 \
+				     -a $combProFile -b $PIPELINE_DIR/$blacklistFileName \
 				     >$combProFile.tmp
+	if [[ "$?" != 0 ]]
+	then
+		echo "There was a non-zero exit code intersecting with bedfile" 
+		exit 2
+	fi
+
 
 	#smooth Data
 	${PYTHON_BINARY} ${TOOL_REMOVE_BREAKPOINTS} -f $combProFile.tmp -o $combProFile.tmp.tmp && mv $combProFile.tmp.tmp $combProFile.tmp && \
 	${PYTHON_BINARY} ${TOOL_MERGE_ARTIFACTS} -f $combProFile.tmp -o $combProFile.tmp.tmp && mv $combProFile.tmp.tmp $combProFile.tmp && \
 	${PYTHON_BINARY} ${TOOL_REMOVE_BREAKPOINTS} -f $combProFile.tmp -o $combProFile.tmp.tmp && mv $combProFile.tmp.tmp $combProFile.tmp && \
+	if [[ "$?" != 0 ]]
+	then
+		echo "There was a non-zero exit code while removing breakpoints" 
+		exit 2
+	fi
 
-	cp $combProFile.tmp $combProFileNoArtifacts
+
+	(head -1 $combProFile.tmp ; tail -n +2 $combProFile.tmp | sort -k 1,1 -V -k 2,2n ) \
+		>$combProFileNoArtifacts
+
+	if [[ "$?" != 0 ]]
+	then
+		echo "There was a non-zero exit code while sorting segment file" 
+		exit 2
+	fi
+
 	#this file could be written out and sorted according to chromosomes
 	${PYTHON_BINARY} ${TOOL_SMOOTH_DATA} -f $combProFile.tmp  -o $combProFile.tmp.tmp && mv $combProFile.tmp.tmp $combProFile.tmp && \
 	${PYTHON_BINARY} ${TOOL_REMOVE_BREAKPOINTS} -f $combProFile.tmp -o $combProFile.tmp.tmp && mv $combProFile.tmp.tmp $combProFile.tmp
+	if [[ "$?" != 0 ]]
+	then
+		echo "There was a non-zero exit code while smoothing segments" 
+		exit 2
+	fi
 
 	patientsex=`cat $SEX_FILE`
 
@@ -44,8 +65,24 @@ do
 		--segmentfile $combProFileNoArtifacts \
 		--mergedfile $combProFile.tmp \
 		--outfile $HRDFile.tmp \
-		--pipelineDir `dirname ${TOOL_HRD_ESTIMATION}`
+		--pipelineDir $PIPELINE_DIR
 
-	mv $combProFile.tmp $combProFileNew
+	if [[ "$?" != 0 ]]
+	then
+		echo "There was a non-zero exit code while estimating HRD score" 
+		exit 2
+	fi
+
+	(head -1 $combProFile.tmp ; tail -n +2 $combProFile.tmp | sort -k 1,1 -V -k 2,2n ) \
+		>$combProFileNew
+
+	if [[ "$?" != 0 ]]
+	then
+		echo "There was a non-zero exit code while sorting segment file" 
+		exit 2
+	fi
+
 	mv $HRDFile.tmp $HRDFile
 done
+
+touch $FILENAME_CHECKPOINT_HRD 
