57e5c3f74a9ae23d2a0a4876ee03ac896b8ec9fe
Kleinheinz <k.kleinheinz@dkfz-heidelberg.de>
2017-07-26 22:18:12 +0200

    fix typo in HRDScoreEstimation

--- src/de/dkfz/b080/co/aceseq/ACEseqConstants.java
+++ src/de/dkfz/b080/co/aceseq/ACEseqConstants.java
@@ -33,7 +33,7 @@ public final class ACEseqConstants {
     public static final String TOOL_ESTIMATE_PEAKS_FOR_PURITY = "estimatePeaksForPurity";
     public static final String TOOL_ESTIMATE_PURITY_AND_PLOIDY = "estimatePurityAndPloidy";
     public static final String TOOL_GENERATE_RESULTS_AND_PLOTS = "generateResultsAndPlots";
-    public static final String TOOL_ESTIMATE_HRD_SCORE = "estimateHrdSore";
+    public static final String TOOL_ESTIMATE_HRD_SCORE = "estimateHrdScore";
     
     private ACEseqConstants() {
     }
