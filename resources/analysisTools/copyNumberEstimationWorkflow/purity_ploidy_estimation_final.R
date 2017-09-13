ce37e416aadc84a87c78d18a3361a770a16f7d89
warsow <g.warsow@dkfz.de>
2017-09-13 18:20:13 +0200

    added contour lines to star plot

--- resources/analysisTools/copyNumberEstimationWorkflow/purity_ploidy_estimation_final.R
+++ resources/analysisTools/copyNumberEstimationWorkflow/purity_ploidy_estimation_final.R
@@ -708,9 +708,28 @@ names(testPlot) = c("purity", "distance", "ploidy")
 testPlot$purity = substring(testPlot$purity, 2, 5)
 testPlot$purity = as.numeric(testPlot$purity)
 
+
+# contour lines will be plotted for each contour_level (e.g. at 101% or 105% of the minimum distance value)
+contour_levels = c(0.01,0.05,0.1,0.15,0.2)
+secondLastLevel = tail(contour_levels,2)[-2]
+countour_local_minima = final_local_minima
+# for contour plots: remove local minima that are not at least 10% larger (or whatever 
+# the third-last value of contour_levels is) than the global minimum
+# that is: no own cloud for minima with near-optimum distance value.
+while (any(abs(diff(countour_local_minima)) <= min(countour_local_minima)*secondLastLevel)) {
+  sel = which(diff(countour_local_minima) <= min(countour_local_minima)*secondLastLevel)[1]
+  sel = c(sel, sel+1)
+  s = which.min( countour_local_minima[sel] )
+  countour_local_minima = countour_local_minima[sel[s]]
+}
+contour_breaks = as.numeric(sapply(countour_local_minima, function(x) {
+  return(x*(1+contour_levels))
+}))
+
 png(qq("@{out}/@{pid}_tcn_distances_combined_star.png"), width = 800, height = 800, type='cairo')
-erupt = ggplot(testPlot, aes(purity, ploidy, fill = distance)) +
-	geom_tile()+ 
+erupt = ggplot(testPlot, aes(purity, ploidy, z = distance)) +
+	geom_tile(aes(fill = distance))+ 
+  stat_contour( aes(colour = ..level..), breaks=contour_breaks) +
 	scale_x_continuous(expand = c(0, 0)) + 
 	scale_y_continuous(expand = c(0, 0)) 
 print( erupt + scale_fill_gradient2(limits = c(0, mean(testPlot$distance[which(!is.na(testPlot$distance))])), midpoint = mean(testPlot$distance[which(!is.na(testPlot$distance))]) / 2, low = "darkred", high = "darkblue", na.value = "darkblue") + 
