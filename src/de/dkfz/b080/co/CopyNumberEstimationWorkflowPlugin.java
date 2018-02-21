a3032c3aed29d13ef65b0f3d0b58d2b5d324b565
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2018-02-21 15:55:48 +0100

    Recompile for Roddy 3.0

--- src/de/dkfz/b080/co/CopyNumberEstimationWorkflowPlugin.java
+++ src/de/dkfz/b080/co/CopyNumberEstimationWorkflowPlugin.java
@@ -8,8 +8,8 @@ import de.dkfz.roddy.plugins.BasePlugin;
  */
 public class CopyNumberEstimationWorkflowPlugin extends BasePlugin {
 
-    public static final String CURRENT_VERSION_STRING = "1.2.10";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Wed Jan 31 15:56:06 CET 2018";
+    public static final String CURRENT_VERSION_STRING = "1.2.8";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Wed Feb 21 15:55:29 CET 2018";
 
     @Override
     public String getVersionInfo() {
