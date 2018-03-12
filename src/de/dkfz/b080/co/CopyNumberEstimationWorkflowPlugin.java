f8b8bf7cfee60eb2ec4efb9af9c2ffdaef69f19f
warsow <g.warsow@dkfz.de>
2018-03-12 15:02:42 +0100

    use COWorkflowsBasePlugin version 1.0.2

--- src/de/dkfz/b080/co/CopyNumberEstimationWorkflowPlugin.java
+++ src/de/dkfz/b080/co/CopyNumberEstimationWorkflowPlugin.java
@@ -15,7 +15,7 @@ import de.dkfz.roddy.plugins.BasePlugin;
 public class CopyNumberEstimationWorkflowPlugin extends BasePlugin {
 
     public static final String CURRENT_VERSION_STRING = "1.2.10";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Fri Mar 09 13:01:32 CET 2018";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Mon Mar 12 14:16:58 CET 2018";
 
     @Override
     public String getVersionInfo() {
