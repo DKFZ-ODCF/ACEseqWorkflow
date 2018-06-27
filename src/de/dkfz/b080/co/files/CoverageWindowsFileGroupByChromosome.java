6eed3bb03313ff4507df55e3b3cf68cf334a69b6
Michael Heinold <m.heinold@dkfz-heidelberg.de>
2018-06-27 14:47:12 +0200

    Modernize some ACEseq code
    This uses the more simplified and easier call with output file group
    syntax for the annotate cnv step.
    
    Also the annotate cnv tool is now properly configured.

--- src/de/dkfz/b080/co/files/CoverageWindowsFileGroupByChromosome.java
+++ src/de/dkfz/b080/co/files/CoverageWindowsFileGroupByChromosome.java
@@ -12,6 +12,8 @@ import de.dkfz.roddy.execution.jobs.BEJobResult;
 import de.dkfz.roddy.execution.jobs.Job;
 import de.dkfz.roddy.knowledge.files.BaseFile;
 import de.dkfz.roddy.knowledge.files.FileGroup;
+import de.dkfz.roddy.knowledge.files.GenericFileGroup;
+import de.dkfz.roddy.knowledge.methods.GenericMethod;
 
 import java.io.File;
 import java.util.LinkedList;
@@ -35,34 +37,9 @@ public class CoverageWindowsFileGroupByChromosome extends FileGroup {
     }
 
     public CoverageWindowsFileAnnotationResult annotate() {
-        List<TextFile> listOfFiles = new LinkedList<>();
-        List<BaseFile> filesToCheck = new LinkedList<>();
+        GenderFile genderFile = (GenderFile)BaseFile.constructManual(GenderFile.class, files.get("1"), null, null, null, null, "genderFile", null, null);
+        GenericFileGroup outFiles = GenericMethod.callGenericToolWithFileGroupOutput(ACEseqConstants.TOOL_ANNOTATE_COV_WIN,  genderFile, new LinkedList<>(files.keySet()), this);
 
-        for (String chrIndex : files.keySet()) {
-            BaseFile src = files.get(chrIndex);
-            TextFile tf = (TextFile) BaseFile.constructGeneric(TextFile.class,
-                    src, null, null, null,
-                    "FILENAME_BREAKPOINTS",
-                    "annotatedCoverage",
-                    chrIndex, src.getFileStage(), null);
-            listOfFiles.add(tf);
-            filesToCheck.add(tf);
-        }
-
-        GenderFile genderFile = (GenderFile)BaseFile.constructManual(GenderFile.class, files.get("1"));
-        genderFile.overrideFilenameUsingSelectionTag("genderFile");
-        filesToCheck.add(genderFile);
-
-        ExecutionContext run = getExecutionContext();
-        Map<String, Object> parameters = run.getDefaultJobParameters(ACEseqConstants.TOOL_ANNOTATE_COV_WIN);
-        parameters.put("FILENAME_SEX", genderFile.getAbsolutePath());
-
-        Job job = new Job(run, run.createJobName((BaseFile) getFilesInGroup().get(0), ACEseqConstants.TOOL_ANNOTATE_COV_WIN, true), ACEseqConstants.TOOL_ANNOTATE_COV_WIN, null, parameters, (List<BaseFile>) getFilesInGroup(), filesToCheck);
-        BEJobResult jobResult = job.run();
-        for (BaseFile baseFile : filesToCheck) {
-            baseFile.setCreatingJobsResult(jobResult);
-        }
-
-        return new CoverageWindowsFileAnnotationResult(listOfFiles, genderFile);
+        return new CoverageWindowsFileAnnotationResult(outFiles.getFilesInGroup(), genderFile);
     }
 }
