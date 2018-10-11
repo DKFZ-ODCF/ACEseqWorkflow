DKFZ-ODCF/ACEseqWorkflow

commit b6274878e3d1d08270d2a96ef89b4c214c90f2f7

zip output file plots/all_corrected.txt

resources/analysisTools/copyNumberEstimationWorkflow/correctGCBias.R
@@ -487,7 +487,7 @@ if (plot_flag) {
   dev.off()
 
 write.table(out_table, outfile, row.names=FALSE, col.names=TRUE,sep='\t', quote=F)
-write.table(sub_order_file, paste0(plotDir,"/all_corrected.txt"), row.names=FALSE, col.names=TRUE,sep='\t', quote=F)
+write.table(sub_order_file, paste0("gzip >",plotDir,"/all_corrected.txt"), row.names=FALSE, col.names=TRUE,sep='\t', quote=F)
 
 cat("creating plots...\n\n")
 png(file=outputfile_gc, width=1000, height=2000, type='cairo')

commit c8849530ddc884fa042da8a4d7c31d9c15eccc9d

fixed bug introduced in b627487

resources/analysisTools/copyNumberEstimationWorkflow/correctGCBias.R
@@ -487,7 +487,7 @@ if (plot_flag) {
   dev.off()
 
 write.table(out_table, outfile, row.names=FALSE, col.names=TRUE,sep='\t', quote=F)
-write.table(sub_order_file, paste0("gzip >",plotDir,"/all_corrected.txt"), row.names=FALSE, col.names=TRUE,sep='\t', quote=F)
+write.table(sub_order_file, pipe( paste0("gzip >",plotDir,"/all_corrected.txt.gz") ), row.names=FALSE, col.names=TRUE,sep='\t', quote=F)
 
 cat("creating plots...\n\n")
 png(file=outputfile_gc, width=1000, height=2000, type='cairo')