cfec6c9d02db61bf4a6b278fe2f055a0467b953d
tomkau93 <tkau93@gmail.com>
2019-12-18 19:38:39 +0100

    Made Beagle helper scripts more readable and included assertion statements to catch incorrect inpute files

--- resources/analysisTools/copyNumberEstimationWorkflow/beagle_embed_haplotypes_vcf.py
+++ resources/analysisTools/copyNumberEstimationWorkflow/beagle_embed_haplotypes_vcf.py
@@ -18,6 +18,9 @@ else:
 vcf_file = open( args.vcf_file, "r" )
 outfile = open( args.out_file, "w" )
 
+SAMPLE_COL = 9
+GT_COL = 8
+
 hap_line = hap_file.readline()
 while is_comment_line(hap_line):
     hap_line = hap_file.readline()
@@ -26,32 +29,38 @@ if hap_line:
     hap_line = hap_line.rstrip().split()
 
 for vcf_line in vcf_file:
+
+    if is_comment_line(vcf_line):
+        if vcf_line.startswith("#CHROM"):
+            vcf_line = vcf_line.rstrip().split("\t")
+            assert len(vcf_file) == 11, "vcf file should have 11 columns"
+            assert vcf_line[10] == "sample0", "The last column should be called 'sample0'"
     
-    if not is_comment_line(vcf_line):
+    else:
         
         vcf_line = vcf_line.rstrip().split("\t")
         vcf_line[0] = vcf_line[0].replace('chr', '')
         vcf_line[0] = vcf_line[0].replace( 'X', '23' )
 
-        if len(vcf_line) >= 9 and hap_line:
+        if len(vcf_line) == 10 and hap_line:
             
             while hap_line and int(hap_line[1]) < int(vcf_line[1]):
                 hap_line = hap_file.readline()
-                
+        
                 if hap_line:
                     hap_line = hap_line.rstrip().split()
                     
             if hap_line and int(hap_line[1]) == int(vcf_line[1]):
                 
-                format_field = hap_line[8].split(":")
+                format_field = hap_line[GT_COL].split(":")
                 genotype_pos_hap = format_field.index( "GT" )
-                format_field = vcf_line[8].split(":")
+                format_field = vcf_line[GT_COL].split(":")
                 genotype_pos_vcf = format_field.index( "GT" )
-                vcf_line[9] = vcf_line[9].split(":")
-                hap_line[9] = hap_line[9].split(":")
+                vcf_line[SAMPLE_COL] = vcf_line[SAMPLE_COL].split(":")
+                hap_line[SAMPLE_COL] = hap_line[SAMPLE_COL].split(":")
 
-                vcf_line[9][genotype_pos_vcf] = hap_line[9][genotype_pos_hap]
-                vcf_line[9] = ":".join( vcf_line[9] )
+                vcf_line[SAMPLE_COL][genotype_pos_vcf] = hap_line[SAMPLE_COL][genotype_pos_hap]
+                vcf_line[SAMPLE_COL] = ":".join( vcf_line[SAMPLE_COL] )
                 
         vcf_line = "\t".join( vcf_line )+"\n"
         
