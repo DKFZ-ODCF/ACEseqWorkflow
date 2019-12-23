94b4eb4d229df70ac9756c62cc7915fec0001352
tomkau93 <tkau93@gmail.com>
2019-12-23 17:37:44 +0100

    Changes in the phasing routine to fix crashes of the workflow. Includes the update of the Beagle binary to the newest version (25Nov19).

--- resources/analysisTools/copyNumberEstimationWorkflow/beagle_create_fake_samples.py
+++ resources/analysisTools/copyNumberEstimationWorkflow/beagle_create_fake_samples.py
@@ -18,16 +18,12 @@ for vcf_line in vcf_infile:
     if is_comment_line(vcf_line):
         if vcf_line.startswith("#CHROM"):
             vcf_line = vcf_line.rstrip().split("\t")
-            assert len(vcf_line) == 10, "The input .vcf file should have 10 columns"
             vcf_line.append('sample0')
             vcf_line = "\t".join( vcf_line )+"\n"
 
     else:
         vcf_line = vcf_line.rstrip().split("\t")
-        original_sample = vcf_line[9]
-        # duplicate the original sample to create a fake sample
-        assert "GT" in original_sample, ".vcf file does not have genotype information ('GT') in the following line \n{}".format("\t".join(vcf_line))
-        vcf_line.append(original_sample)
+        vcf_line.append(vcf_line[9])
         vcf_line = "\t".join( vcf_line )+"\n"
 
     outfile.write( vcf_line )
